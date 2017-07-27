21dec3e1f690884e8b7b207c3b4f59355026068c
Xiaolin Ju <Ju.xl@ntu.edu.cn>
2017-07-27 23:13:25 +0800

    OK

--- bin/README.md
+++
@@ -1,2 +0,0 @@
-# Nextday
-Nextday Unittest
---
+++ src/main/java/Nextday_SE15/Nextday/App.java
@@ -0,0 +1,13 @@
+package Nextday_SE15.Nextday;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
